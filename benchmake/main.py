e9b9e7dafc19f28d0b4f50ef981021f1610c4450
felix <sirexeclp@gmail.com>
2019-12-02 23:26:09 +0100

    example benchmark

--- benchmake/main.py
+++ benchmake/main.py
@@ -4,26 +4,53 @@ import subprocess
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-print(sys.argv[1:])
-assert len(sys.argv)>1, "missing arguments"
+from tqdm import tqdm
+import pandas as pd
+# print(sys.argv[1:])
+# assert len(sys.argv)>1, "missing arguments"
 
-timings = []
-reppetitions = 100
-warmup = 3
+def set_boxplot_legend(legend):
+    plt.xticks(*zip(*enumerate(legend,1)))
 
-for i in range(reppetitions+warmup):
-    start = time.time()
-    subprocess.run(sys.argv[1:])
-    end = time.time()
-    timings.append(end-start)
+config = {
+    "name": "ffts_bench_small_n"
+    ,"runs":[
+        # {"args":["../fft/dft", "dft" ,"10"]},
+        {"args":["../fft/dft", "fft" ,"17"]}
+        ,{"args":["../fft/dft", "fftw" ,"17"]}
+    ]
+}
 
-warm_timings = timings[warmup:]
+reppetitions = 10
+warmup = 2
 
-print(f"Mean: {np.mean(warm_timings)} SD:{np.std(warm_timings)}")
-plt.plot(warm_timings)
+runs = []
+for run in config["runs"]:
+    timings = []
+    print(run["args"])
+    for i in tqdm(range(reppetitions+warmup)):
+        start = time.time()
+        subprocess.run(run["args"], stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+        end = time.time()
+        timings.append(end-start)
+    runs.append(timings)
+
+algorithms = [run["args"][1] for run in config["runs"]]
+df = pd.DataFrame(runs, algorithms).transpose()
+df.to_csv(config["name"]+".csv")
+
+for timings,run in zip(runs,config["runs"]):
+    warm_timings = timings[warmup:]
+    print(run["args"])
+    print(f"Mean: {np.mean(warm_timings)} SD:{np.std(warm_timings)}")
+    plt.plot(warm_timings)
 plt.title("Warm Timings")
+plt.legend(algorithms)
+plt.show()
+plt.boxplot(runs)
+set_boxplot_legend(algorithms)
 plt.show()
 
-plt.plot(timings)
-plt.title("ALL Timings")
-plt.show()
+# plt.plot(timings)
+# plt.title("ALL Timings")
+# plt.show()

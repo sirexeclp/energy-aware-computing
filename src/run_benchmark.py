cfd3925169ecc6af84979c300d32ad89c7c7ba63
felix <sirexeclp@gmail.com>
2020-02-06 16:47:36 +0100

    refactor and cleanup + add other nets

--- src/run_benchmark.py
+++ src/run_benchmark.py
@@ -10,16 +10,13 @@ from collections import namedtuple
 from pathlib import Path
 import pandas as pd
 from smi_wrapper import GPU, SMIWrapper, Utilization
-
+from asyncio.subprocess import create_subprocess_exec
+import asyncio
+from random import shuffle
 # %%
 WARMUP = 5 #warmup in seconds
 
-def run_experiment(data_path, args ,baseline = 0, power_limit=None):
-    gpu_data = []
-    
-    data_path.mkdir(parents=True)
-    gpu_data_path = data_path / "gpu-power.csv"
-
+def set_power_limit(power_limit):
     with SMIWrapper() as sw:
         if power_limit is not None:
             success = sw.set_all_powerlimit(power_limit*1000)#convert to mW
@@ -29,89 +26,104 @@ def run_experiment(data_path, args ,baseline = 0, power_limit=None):
             print(f"[{'Success' if success else 'Failed'}] reset power-limit to default (max)")
         
         assert success, "Failed setting/resetting power-limit, abborting experiment!"
+
+async def run_experiment(data_path, working_directory, module, args ,baseline = 0, power_limit=None):
+    data_path.mkdir(parents=True)
+
+    set_power_limit(power_limit)
         
-        if baseline:
-            sw.get_baseline(baseline, gpu_data)
-       
-        p = subprocess.Popen(args)
-        while p.poll() is None:
-            sw.get_all_stats(gpu_data)
-            #time.sleep(0.5-0.0752)
-        
-        df = pd.DataFrame(gpu_data)
-        df.to_csv(gpu_data_path)
+    args = ["gPyJoules.py", "-d", str(data_path.absolute()), "-w", str(working_directory), module, "--"] + args
+    p = await create_subprocess_exec("python3", *args)
+    await p.wait()
 
 
 # %%
 
+def run_power_cap_experiment(module, args, working_directory, power_caps, data_root ,description):
+    data_root = Path(data_root)
+    shuffle(power_caps)
+    for p in power_caps:
+        description = description.format(p)
+        print(f"[Running] {description}")
+        data_path = data_root / Path(f"{description}-{datetime.now().isoformat()}")
+        asyncio.run(run_experiment(data_path, str(working_directory), module, args))
+        time.sleep(10)
 
-def run_batch_experiment():
-    #takes 1h to run on gtx 1060
-    dense_count = [1, 2, 4]
-    dense_sizes = [128, 256]
-    batch_sizes = [8, 16, 32, 64, 128]
-    for c in dense_count:
-        for d in dense_sizes:
-            for b in batch_sizes:
-                description = f"{b}batch{d}denseX{c}"
-                print(f"running {description}")
-                data_root = Path("power-data")
-                data_path = data_root / Path(f"{description}-{datetime.now().isoformat()}")
-                args = ["python3","mnist_cnn.py",str(c), str(d), str(b) , str(data_path)]
-                run_experiment(data_path, args)
+
+def run_power_cap_experiment_ecg(data_root, power_caps):
+    epochs = 20
+    description = "powercap{}-ecg"
+    run_power_cap_experiment("train", ["-n", str(epochs)], "../../ecg/ecg/", power_caps, data_root, description)
+
+def run_power_cap_experiment_mnist(data_root, power_caps):
+    description = "powercap{}-mnist"
+    run_power_cap_experiment("mnist_cnn", ["-c", 2, "-n", 512, "-b", 128], "./", power_caps, data_root, description)
+
+def run_power_cap_experiment_cifar(data_root, power_caps):
+    description = "powercap{}-cifar"
+    run_power_cap_experiment("resnet", [], "./", power_caps, data_root, description)
+
+def run_all_power_cap_corse(data_root):
+    repititions = 10
+    power_caps = [150, 200, 250, 300] #Watts
+    for i in range(repititions):
+        data_path = data_root / f"run{i}"
+        run_power_cap_experiment_ecg(data_path, power_caps)
+        sleep(30)
+        run_power_cap_experiment_mnist(data_path, power_caps)
+        sleep(30)
+        run_power_cap_experiment_cifar(data_path, power_caps)
+        sleep(30)
+# %%
+# def run_batch_experiment():
+#     #takes 1h to run on gtx 1060
+#     dense_count = [1, 2, 4]
+#     dense_sizes = [128, 256]
+#     batch_sizes = [8, 16, 32, 64, 128]
+#     for c in dense_count:
+#         for d in dense_sizes:
+#             for b in batch_sizes:
+#                 description = f"{b}batch{d}denseX{c}"
+#                 print(f"running {description}")
+#                 data_root = Path("power-data")
+#                 data_path = data_root / Path(f"{description}-{datetime.now().isoformat()}")
+#                 args = ["python3","mnist_cnn.py",str(c), str(d), str(b) , str(data_path)]
+#                 run_experiment(data_path, args)
 
 # %%
 #run_batch_experiment()
 
-def run_power_cap_experiment():
-    import random
-    power_caps = [150, 200, 250, 300] #Watts
-    random.shuffle(power_caps)
-    epochs = 20#5
-    for p in power_caps:
-        description = f"powercap{p}"
-        print(f"running {description}")
-        data_root = Path("power-data")
-        data_path = data_root / Path(f"{description}-{datetime.now().isoformat()}")
-        args = ["/bin/bash","./train_cinc17.sh", "-n", str(epochs), "-p", str(data_path)]
+# def run_power_cap_experiment_fine():
+#     import random
+#     power_caps = list(range(150,300,25))#[150, 200, 250, 300] #Watts
+#     random.shuffle(power_caps)
+#     epochs = 5
+#     for p in power_caps:
+#         description = f"powercap{p}"
+#         print(f"running {description}")
+#         data_root = Path("power-data-fine")
+#         data_path = data_root / Path(f"{description}-{datetime.now().isoformat()}")
+#         args = ["/bin/bash","./train_cinc17.sh", "-n", str(epochs), "-p", str(data_path)]
         
-        run_experiment(data_path, args,baseline=5,power_limit=p)
-        time.sleep(10)
+#         run_experiment(data_path, args,baseline=5,power_limit=p)
+#         time.sleep(10)
 
-def run_power_cap_experiment_fine():
-    import random
-    power_caps = list(range(150,300,25))#[150, 200, 250, 300] #Watts
-    random.shuffle(power_caps)
-    epochs = 5
-    for p in power_caps:
-        description = f"powercap{p}"
-        print(f"running {description}")
-        data_root = Path("power-data-fine")
-        data_path = data_root / Path(f"{description}-{datetime.now().isoformat()}")
-        args = ["/bin/bash","./train_cinc17.sh", "-n", str(epochs), "-p", str(data_path)]
-        
-        run_experiment(data_path, args,baseline=5,power_limit=p)
-        time.sleep(10)
-
-def run_batch_experiment2(data_root):
-    dense_count = [8, 16, 32]
-    dense_sizes = [128, 256, 512]
-    batch_sizes = [64, 128, 256, 512]
-    for c in dense_count:
-        for d in dense_sizes:
-            for b in batch_sizes:
-                description = f"{b}batch{d}denseX{c}"
-                print(f"running {description}")
-                data_root = Path(data_root)
-                data_path = data_root / Path(f"{description}-{datetime.now().isoformat()}")
-                args = ["python3","mnist_cnn.py",str(c), str(d), str(b) , str(data_path)]
-                run_experiment(data_path, args)
+# def run_batch_experiment2(data_root):
+#     dense_count = [8, 16, 32]
+#     dense_sizes = [128, 256, 512]
+#     batch_sizes = [64, 128, 256, 512]
+#     for c in dense_count:
+#         for d in dense_sizes:
+#             for b in batch_sizes:
+#                 description = f"{b}batch{d}denseX{c}"
+#                 print(f"running {description}")
+#                 data_root = Path(data_root)
+#                 data_path = data_root / Path(f"{description}-{datetime.now().isoformat()}")
+#                 args = ["-c", str(c), "-n", str(d), "-b", str(b)]
+#                 asyncio.run(run_experiment(data_path, "./", "mnist_cnn", args))
 
 
 if __name__ == "__main__":
-    #run_power_cap_experiment()
-    #time.sleep(120)
-    #run_power_cap_experiment_fine()
     run_batch_experiment2("../testing/data")
 
     with SMIWrapper() as sw:

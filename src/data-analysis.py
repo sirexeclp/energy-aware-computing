87a4d39deac6153b03303d7a081fdfcef2591e57
felix <sirexeclp@gmail.com>
2020-02-10 22:11:45 +0100

    multi runs

--- src/data-analysis.py
+++ src/data-analysis.py
@@ -14,6 +14,10 @@
 # ---
 
 # %%
+%load_ext autoreload
+
+%autoreload 2
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -26,8 +30,6 @@ unit = pint.UnitRegistry()
 mpl.rcParams['figure.dpi'] = 300
 
 from util import *
-# %%
-power_data = PowerData.load("power-data/8batch128denseX1-2020-01-26 22:45:57.471455")
 
 
 # %% [markdown]
@@ -291,3 +293,246 @@ plt.plot(power_cap_data.power_caps, power_cap_data.time/np.timedelta64(1, 's'))
 
 # %% [markdown]
 #
+
+
+# %%
+GPU1=1
+devices=[GPU1]
+power_caps = [150, 200, 250, 300]
+
+#%%
+def load_power_cap_data(data_root, network, epoch_count):
+    
+    data_root = Path(data_root)
+    runs = list(data_root.glob("run*"))
+
+    all_data = []
+
+    for cap in power_caps:
+        for run in runs:
+            description = f"powercap{cap}-{network}"
+            #print(description)
+            data_path = list(run.glob(f"{description}*"))[0]
+            power_data = PowerData.load(data_path)
+            assert power_data.t_info.get_epoch_count() == epoch_count, "Unexpected Number of epochs!"
+            epoch_energy = get_energy_per_epoch(power_data, devices=devices)
+            energy = np.mean(epoch_energy[1:])
+           
+            epoch_times = power_data.t_info.get_time_per_epoch()
+            mean_time = np.mean(epoch_times[1:])
+            
+            total_time = power_data.t_info.get_total_experiment_duration() / np.timedelta64(1,"s")
+            total_energy = calculate_total_energy(power_data, devices=devices
+                            ,start=power_data.t_info.get_train_begin()
+                            , end=power_data.t_info.get_train_end())
+            
+            edp = total_energy * total_time
+
+            cum_energy = calculate_total_cumulative_energy(power_data, devices=devices
+                            ,start=power_data.t_info.get_train_begin()
+                            , end=power_data.t_info.get_train_end())[0]
+
+            tmp = {
+                "power_cap":cap
+                ,"energy":energy
+                ,"epoch_energy":epoch_energy
+                ,"mean_time":mean_time
+                ,"total_time":total_time
+                ,"epoch_times":epoch_times
+                ,"total_energy":total_energy
+                ,"cum_energy": cum_energy
+                ,"edp": edp
+                ,"run": str(run).split("/")[-1].replace("run","")
+                ,"power_data": power_data
+            }
+            all_data.append(tmp)
+    return all_data
+# %%
+mnist_data_big = load_power_cap_data("../data/data-1.2", "mnist-big", epoch_count = 10)
+mnist_data_big = pd.DataFrame(mnist_data_big)
+
+# %%plot_epoch_times
+    fig, ax = plt.subplots(len(power_caps), 1, sharex=True)
+    for index,(_, pl) in enumerate(df[df.run=="1"].iterrows()):
+        current_ax = ax[index]
+        power_data = pl["power_data"]
+        device = power_data.power_gpu[power_data.power_gpu["gpu-index"] == device_idx]
+        timestamps = (np.array(device.timestamp) - np.array(device.timestamp)[0]) / np.timedelta64(1, "s")
+        current_ax.plot(timestamps, device.power)
+        current_ax.set_ylim(0,310)
+
+        for i, epoch_begin, _ in power_data.t_info.epochs():
+            epoch_ts = (epoch_begin - np.array(device.timestamp)[0]) / np.timedelta64(1, "s")
+            current_ax.axvline(x=epoch_ts,color='orange',linestyle='--')
+    current_ax.set_ylabel("Power [W]")
+    current_ax.set_xlabel("Time [s]")
+    fig.suptitle("GPU Power vs. Time  w/ Power Limits [150, 200 ,250, 300]")
+    #plt.tight_layout() 
+    plt.show()
+
+
+plot_power_raw(mnist_data_big, devices[0])
+# %%
+
+def plot_cum_energy(df, net):
+    runs = df.groupby("run")
+    for run_idx, run in runs:
+        pl_list =[]
+        for pl_idx, pl in run.groupby("power_cap"):
+            pl = pl.iloc[0]
+            pl_list.append(pl_idx)
+            epoch2_begin = pl.power_data.t_info.get_epoch_begin(1)
+
+            plt.plot(pl.cum_energy/MEGA)
+            #plt.plot((epoch2_begin-pl.power_data.power_gpu.iloc[0].timestamp)/np.timedelta64(1,"s"),0,"x")
+        plt.legend([f"{x}W" for x in pl_list])
+        plt.title(f"[{net}]Cumulative Energy w/ Power Limits (Run {run_idx})")
+        plt.xlabel("Time [$s$]")
+        plt.ylabel("Energy [$MJ$]")
+        plt.show()
+
+plot_cum_energy(mnist_data_big)
+
+# %%
+def epoch_times_hist(df):
+    times = {}
+    for pl_idx, pl in df.groupby("power_cap"):
+        times[pl_idx] = []
+        for run_idx, run in pl.groupby("run"):
+            run = run.iloc[0]
+            times[pl_idx].extend(run.epoch_times[1:])
+            
+        plt.hist(times[pl_idx], bins=30)
+        plt.title(f"[{net}]Distribution of Time per Epoch ({pl_idx}W)")
+        plt.show()
+
+
+epoch_times_hist(mnist_data_big)
+
+
+# %%
+def epoch_times_boxplot(df):
+    times = {}
+    for pl_idx, pl in df.groupby("power_cap"):
+        times[pl_idx] = []
+        for run_idx, run in pl.groupby("run"):
+            run = run.iloc[0]
+            times[pl_idx].extend(run.epoch_times[1:])
+            
+    plt.boxplot(times.values())
+    plt.xticks(*zip(*enumerate([f"{x}W" for x in times.keys()],1)))
+    plt.title(f"[{net}]Time per Epoch")
+    plt.ylabel("Time [s]")
+    plt.show()
+
+
+epoch_times_boxplot(mnist_data_big)
+
+
+
+# %%
+def epoch_energy_hist(df):
+    times = {}
+    for pl_idx, pl in df.groupby("power_cap"):
+        times[pl_idx] = []
+        for run_idx, run in pl.groupby("run"):
+            run = run.iloc[0]
+            times[pl_idx].extend(run.epoch_energy[1:])
+            
+        plt.hist(times[pl_idx], bins=30)
+        plt.title(f"[{net}]Distribution of Energy per Epoch ({pl_idx}W)")
+        plt.show()
+
+
+epoch_energy_hist(mnist_data_big)
+
+
+# %%
+def epoch_energy_boxplot(df):
+    times = {}
+    for pl_idx, pl in df.groupby("power_cap"):
+        times[pl_idx] = []
+        for run_idx, run in pl.groupby("run"):
+            run = run.iloc[0]
+            times[pl_idx].extend(run.epoch_energy[1:])
+            
+    plt.boxplot(times.values())
+    plt.xticks(*zip(*enumerate([f"{x}W" for x in times.keys()],1)))
+    plt.title(f"[{net}]Energy per Epoch")
+    plt.ylabel("Energy [J]")
+    plt.show()
+
+
+epoch_energy_boxplot(mnist_data_big)
+
+#%%
+def plot_mean_total_energy(df):
+    energy = {}
+    for pl_idx, pl in df.groupby("power_cap"):
+        energy[pl_idx] = []
+        for run_idx, run in pl.groupby("run"):
+            run = run.iloc[0]
+            energy[pl_idx].append(run.total_energy)
+            
+    plt.plot([x for x in energy.keys()], np.array([np.array(x).mean() for x  in energy.values()]) / MEGA, "x")
+    plt.title(f"[{net}]Mean Total Energy vs. Power Limit")
+    plt.xlabel("Power Limit [W]")
+    plt.ylabel("Energy [MJ]")
+    plt.xticks(power_caps)
+    plt.show()
+
+
+plot_mean_total_energy(mnist_data_big)
+
+
+#%%
+def plot_mean_edp(df):
+    energy = {}
+    for pl_idx, pl in df.groupby("power_cap"):
+        energy[pl_idx] = []
+        for run_idx, run in pl.groupby("run"):
+            run = run.iloc[0]
+            energy[pl_idx].append(run.edp)
+            
+    plt.plot([x for x in energy.keys()], np.array([np.array(x).mean() for x  in energy.values()]) / MEGA, "x")
+    plt.title(f"[{net}]Mean EDP vs. Power Limit")
+    plt.xlabel("Power Limit [W]")
+    plt.ylabel("Energy Delay Product [MJs]")
+    plt.xticks(power_caps)
+    plt.show()
+
+
+plot_mean_edp(mnist_data_big)
+
+
+#%%
+def plot_mean_total_time(df):
+    energy = {}
+    for pl_idx, pl in df.groupby("power_cap"):
+        energy[pl_idx] = []
+        for run_idx, run in pl.groupby("run"):
+            run = run.iloc[0]
+            energy[pl_idx].append(run.total_time)
+            
+    plt.plot([x for x in energy.keys()], np.array([np.array(x).mean() for x  in energy.values()]), "x")
+    plt.title(f"[{net}]Mean Total Time vs. Power Limit")
+    plt.xlabel("Power Limit [W]")
+    plt.ylabel("Time [s]")
+    plt.xticks(power_caps)
+    plt.show()
+
+
+plot_mean_total_time(mnist_data_big)
+#%%
+# acc = 0
+# for dev in devices:
+#     dev_data = df[df.]
+#     acc +=  get_cumulative_energy()
+# tmp = calculate_total_cumulative_energy(df.power_data, devices=devices
+#                     ,start=power_data.t_info.get_train_begin()
+#                     , end=power_data.t_info.get_train_end())[0]
+        
+#cum_energy = get_cumulative_energy(gpu0.power, gpu0.timestamp)
+#plt.plot(gpu0.timestamp,cum_energy)
+
+# %%

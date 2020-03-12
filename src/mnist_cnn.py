b306d4d991da494b66782d7f7cdf96ddf7268347
felix <sirexeclp@gmail.com>
2020-03-12 19:10:41 +0100

    skip evaluation

--- src/mnist_cnn.py
+++ src/mnist_cnn.py
@@ -89,6 +89,6 @@ model.fit(x_train, y_train,
           verbose=1,
           validation_data=(x_test, y_test)
           )
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+# score = model.evaluate(x_test, y_test, verbose=0)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])

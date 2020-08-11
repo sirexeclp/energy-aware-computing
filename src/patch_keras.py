17f7d86dff5ae97f191d9b9f47cf3deab74a2f8b
felix <sirexeclp@gmail.com>
2020-08-11 13:35:27 +0200

    multiprocess all the things

--- src/patch_keras.py
+++ src/patch_keras.py
@@ -1,28 +1,30 @@
+import abc
+import multiprocessing
 import warnings
 
 # with warnings.catch_warnings():
-from pynvml3 import Device
+from typing import Union
+
+from pynvml3 import Device, ClockType, ClockId
 from pynvml3 import TemperatureSensors, PcieUtilCounter, SamplingType
 from pynvml3 import NVMLLib
 from pynvml3.errors import NVMLErrorNotFound
 
 warnings.filterwarnings('ignore')
+
 import tensorflow
 import tensorflow.keras as keras
 import atexit
 from multiprocessing import Process, Event, Queue
-from time import sleep, time
-from .util import *
-
-stop_event = Event()
-data_event = Event()
-data_queue = Queue()
+import time
+from util import *
 
 
 class EnergyCallback(keras.callbacks.Callback):
     """Costum Keras Callback to log events, for energy measurements."""
 
-    def __init__(self, enable_energy_prediction, num_epochs, log_path, visible_devices):
+    def __init__(self, enable_energy_prediction,
+                 num_epochs, log_path, visible_devices, data_event, data_queue):
         super(EnergyCallback, self).__init__()
         self.timestamp_log = []
         self.enable_energy_prediction = enable_energy_prediction
@@ -31,8 +33,10 @@ class EnergyCallback(keras.callbacks.Callback):
         self.columns = ["timestamp", "event", "data"]
         self.total_batch = 0
         self.visible_devices = visible_devices
-        self.last_time = time()
+        self.last_time = time.time()
         self.summary_writer = tensorflow.summary.create_file_writer("/tmp/energy-board/energy")
+        self.data_event = data_event
+        self.data_queue = data_queue
 
     def on_train_batch_end(self, batch, logs=None):
         pass
@@ -72,12 +76,11 @@ class EnergyCallback(keras.callbacks.Callback):
         }
         self.timestamp_log.append(tmp)
 
-    @staticmethod
-    def get_gpu_data():
-        data_event.set()
-        while data_queue.empty():
-            sleep(0.1)
-        gpu_data = data_queue.get()
+    def get_gpu_data(self):
+        self.data_event.set()
+        while self.data_queue.empty():
+            time.sleep(0.1)
+        gpu_data = self.data_queue.get()
         gpu_data = pd.DataFrame(gpu_data)
         return gpu_data
 
@@ -105,95 +108,184 @@ def get_log_path(new_path):
     return timestamp_log_path
 
 
-def get_all_stats(device: Device):
-    data = {
-        "gpu-index": device.get_index()
-        , "timestamp": str(datetime.now())
-        # , "util-gpu": self.get_sample(SampleType.GpuUtilization)
-        # , "util-mem": self.get_sample(SampleType.MemoryUtilization)
-        #
-        # , "clock-mem": self.get_sample(SampleType.MemoryClock)
-        # , "clock-gpu": self.get_sample(SampleType.GpuClock)
-        #
-        , "power-state": device.get_power_state()
-        #
-        # , "power": self.get_sample(SampleType.TotalPower)
-        , "power": device.get_power_usage()
-        , "tmp": device.get_temperature(TemperatureSensors.TEMPERATURE_GPU)
-        , "pci-tx": device.get_pcie_throughput(PcieUtilCounter.TX_BYTES)
-        , "pci-rx": device.get_pcie_throughput(PcieUtilCounter.RX_BYTES)
-    }
-    return data
+class ProcessTimer(abc.ABC):
+
+    def __init__(self, interval, args=None, kwargs=None):
+        self.interval = interval
+        self.args = () if args is None else args
+        self.kwargs = {} if kwargs is None else kwargs
+        self._stop = multiprocessing.Event()
+        self.last_time = None
+        self.process = multiprocessing.Process(target=self._run)
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+
+    def start(self):
+        self.last_time = time.time()
+        self._stop.clear()
+        self.process.start()
+
+    def stop(self):
+        self._stop.set()
+
+    @abc.abstractmethod
+    def _on_start(self):
+        pass
+
+    @abc.abstractmethod
+    def _on_stop(self):
+        pass
+
+    @abc.abstractmethod
+    def _on_tick(self, args, kwargs):
+        pass
+
+    def _run(self):
+        iter_start = time.time()
+        self._on_start()
+        while not self._stop.is_set():
+            self._on_tick(self.args, self.kwargs)
+            now = time.time()
+            sleep_time = self.interval - (now - self.last_time)
+            if (now - iter_start) > self.interval:
+                print(f"WARNING: can't keep up { (now - iter_start)}s")
+                # TODO: log that we are running late
+            self.last_time = now
+            time.sleep(max(sleep_time, 0))
+            self._run()
+        self._on_stop()
 
 
-class SampleCollector:
-
-    def __init__(self, sample_type):
-        self.sample_type = sample_type
+class Collector(ProcessTimer):
+    def __init__(self, device_id, interval, path, args=None, kwargs=None):
+        super(Collector, self).__init__(interval, args, kwargs)
         self.data = []
-        self.last_time = 0
+        self.device_id = device_id
+        self.lib = None
+        self.device: Device = None
+        self.path = Path(path)
 
-    def collect(self, device):
-        samples = device.get_samples(self.sample_type, self.last_time)
-        self.data.extend(samples)
-        self.last_time = samples[-1].timestamp
+    def stop(self):
+        super(Collector, self).stop()
+        self._save()
 
-    def to_csv(self, path):
-        path = Path(path) / (self.sample_type.get_filename() + ".csv")
+    def _on_start(self):
+        self.lib = NVMLLib()
+        self.lib.open()
+        self.device = self.lib.device.from_index(self.device_id)
+
+    def _on_stop(self):
+        self.device = None
+        self.lib.close()
+
+    @abc.abstractmethod
+    def _get_save_path(self):
+        pass
+
+    def _save(self):
+        path = self._get_save_path()
         df = pd.DataFrame(self.data)
         df.to_csv(path)
+
+
+class SampleCollector(Collector):
+
+    def __init__(self, sample_type, device_id, interval, path):
+        super(SampleCollector, self).__init__(device_id, interval, path)
+        self.sample_type = sample_type
+        self.last_sample_time = 0
+
+    def _on_tick(self, args, kwargs):
+        samples = self.device.get_samples(self.sample_type, self.last_sample_time)
+        self.data.extend(samples)
+        self.last_sample_time = samples[-1].timestamp
+
+    def _get_save_path(self):
+        return self.path / (self.sample_type.get_filename() + ".csv")
 
     def test(self, device):
         try:
             # first call sometimes works but then the second will fail; idk why.
-            self.collect(device)
-            self.collect(device)
+            self.device = device
+            self._on_tick(None, None)
+            self._on_tick(None, None)
             print(f"{self.sample_type}: OK")
+            self.device = None
             return True
         except NVMLErrorNotFound as e:
             print(f"{self.sample_type} not supported on this device!")
+            self.device = None
             return False
 
 
-def collect_power_data(data_path, done, get_data, data, visible_devices):
-    gpu_data = []
-    collectors = []
-    sampling_types = [
-        SamplingType.GPU_UTILIZATION_SAMPLES,
-        SamplingType.MEMORY_UTILIZATION_SAMPLES,
-        SamplingType.MEMORY_CLK_SAMPLES,
-        SamplingType.PROCESSOR_CLK_SAMPLES,
-        SamplingType.TOTAL_POWER_SAMPLES]
+class SlowCollector(Collector):
 
-    data_path = Path(data_path)
-    data_path.mkdir(parents=True, exist_ok=True)
-    gpu_data_path = data_path / "gpu-power.csv"
+    def _get_save_path(self):
+        return self.path / "gpu-power.csv"
 
-    with NVMLLib() as lib:
-        device = lib.device.from_index(visible_devices)
+    def _on_tick(self, args, kwargs):
+        data_event, data_queue = args
+        util = self.device.get_utilization_rates()
+        data = {
+            "timestamp": str(datetime.now())
+            , "util-gpu": util.gpu
+            , "util-mem": util.memory
 
-        # collector = [c:=SampleCollector(st) for st in sampling_types if c.test(device)]
-        for st in sampling_types:
-            collector = SampleCollector(st)
-            if collector.test(device):
-                collectors.append(collector)
+            , "clock-mem": self.device.get_clock(ClockType.MEM, ClockId.CURRENT)
+            , "clock-gpu": self.device.get_clock(ClockType.SM, ClockId.CURRENT)
+            #
+            , "power-state": self.device.get_power_state().value  # int representation to save on storage size
+            #
+            , "power": self.device.get_power_usage()
+            , "tmp": self.device.get_temperature(TemperatureSensors.TEMPERATURE_GPU)
+            , "pci-tx": self.device.get_pcie_throughput(PcieUtilCounter.TX_BYTES)
+            , "pci-rx": self.device.get_pcie_throughput(PcieUtilCounter.RX_BYTES)
+        }
+        self.data.append(data)
+        if not data_event.is_set():
+            data_queue.put(self.data)
+            data_event.set()
 
-        print("start recording")
-        while not done.is_set():
-            gpu_data.append(get_all_stats(device))
-            for collector in collectors:
-                collector.collect(device)
 
-            if get_data.is_set():
-                get_data.clear()
-                data.put(gpu_data)
-            sleep(1)
+class CollectorManager:
+    def __init__(self, data_path, device_id, ):
+        self.collectors: List[Collector] = []
+        self.data_path = Path(data_path)
+        self.data_path.mkdir(parents=True, exist_ok=True)
+        self.device_id = device_id
 
-        df = pd.DataFrame(gpu_data)
-        df.to_csv(gpu_data_path)
-        df.mean()
-        for collector in collectors:
-            collector.to_csv(data_path)
+    def add(self, collector: Union[Collector, List[Collector]]):
+        if isinstance(collector, list):
+            self.collectors.extend(collector)
+        else:
+            self.collectors.append(collector)
+
+    def add_by_sampling_type(self, sampling_type: Union[SamplingType,
+                                                        List[SamplingType]]
+                             , interval):
+        if isinstance(sampling_type, SamplingType):
+            sampling_type = [sampling_type]
+
+        with NVMLLib() as lib:
+            device = lib.device.from_index(self.device_id)
+            for st in sampling_type:
+                collector = SampleCollector(st, self.device_id, interval
+                                            , self.data_path)
+                if collector.test(device):
+                    self.add(collector)
+
+    def start(self):
+        for col in self.collectors:
+            col.start()
+
+    def stop(self):
+        for col in self.collectors:
+            col.stop()
 
 
 def patch(data_root, enable_energy, visible_devices):
@@ -201,19 +293,36 @@ def patch(data_root, enable_energy, visible_devices):
     # timestamp_log = open(timestamp_log_path , "w", buffering=1)
     # timestamp_log.write(f"timestamp,event,data\n")
 
-    power_process = Process(target=collect_power_data,
-                            args=(data_root, stop_event, data_event, data_queue, visible_devices))
-    power_process.start()
+    sampling_types = [
+        SamplingType.GPU_UTILIZATION_SAMPLES,
+        SamplingType.MEMORY_UTILIZATION_SAMPLES,
+        SamplingType.MEMORY_CLK_SAMPLES,
+        SamplingType.PROCESSOR_CLK_SAMPLES,
+        SamplingType.TOTAL_POWER_SAMPLES
+    ]
+
+    sampling_manager = CollectorManager(data_root, visible_devices)
+    sampling_manager.add_by_sampling_type(sampling_types, interval=1.5)
+
+    data_event = Event()
+    data_queue = Queue()
+
+    sampling_manager.add(SlowCollector(visible_devices,
+                                       interval=0.5,
+                                       path=data_root,
+                                       args=(data_event, data_queue)))
+    sampling_manager.start()
 
     @atexit.register
     def on_exit():
-        stop_event.set()
-        power_process.join()
+        sampling_manager.stop()
 
     def get_patched_fit(original_function):
         def patched_fit(*args, **kwargs):
             num_epochs = kwargs.get("epochs")
-            callback = EnergyCallback(enable_energy, num_epochs, timestamp_log_path, visible_devices)
+            callback = EnergyCallback(enable_energy, num_epochs
+                                      , timestamp_log_path, visible_devices
+                                      , data_event, data_queue)
             kwargs.setdefault("callbacks", list()).append(callback)
             if not enable_energy:
                 kwargs["verbose"] = 2

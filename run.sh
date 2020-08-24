beff50ae44cb2d42fdf8bbc4ca85f9abe1907229
felix.grzelka <felix.grzelka@student.hpi.uni-potsdam.de>
2020-08-24 17:49:03 +0200

    plot and pass NVIDIA_VISIBLE DEVICES to sudo

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 cd $(dirname $0)/src
+echo "NVIDIA_VISIBLE_DEVICES=$NVIDIA_VISIBLE_DEVICES"
 nvidia-smi
-sudo python3 -m run_benchmark
+sudo --preserve-env=NVIDIA_VISIBLE_DEVICES python3 -m run_benchmark

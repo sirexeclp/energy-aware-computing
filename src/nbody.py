63c10040f78b40016262fa7b3a5d30fb9e5d915d
sirexeclp <sirexeclp@gmail.com>
2020-09-18 17:03:20 +0200

    adapt path

--- src/nbody.py
+++ src/nbody.py
@@ -1,3 +1,3 @@
 import subprocess
-r = subprocess.call("~/samples/bin/x86_64/linux/release/nbody -benchmark -numbodies=819200", shell=True)
+r = subprocess.call("./nbody -benchmark -numbodies=819200", shell=True)
 exit(r)

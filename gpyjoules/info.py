67bfb6aca8cd3db778bcaa3994c5ed9b36e9f7c3
Felix Grzelka <felix.grzelka@hpi.de>
2022-08-31 14:55:28 +0200

    refactor info script

--- gpyjoules/info.py
+++ gpyjoules/info.py
@@ -1,27 +1,31 @@
+"""Provide info about available power- and clock-limits."""
 import pynvml3
 import numpy as np
 
-lib = pynvml3.NVMLLib()
-num_total_runs = 10
-gpu_index = 0
-with lib:
-    dev = lib.device.from_index(gpu_index)
-    name = dev.get_name()
-    memory_clocks = dev.get_supported_memory_clocks()
 
-    pl_constraints = dev.get_power_management_limit_constraints()
-    pl_constraints = np.array(pl_constraints) / 1_000  # mW 2 W
+def print_info4gpu(gpu: pynvml3.Device):
+    """Print power and clock limits for the given GPU-Device."""
+    print("GPU:", gpu.get_name())
 
-    print("Power Limit Constraints: ", pl_constraints)
+    pl_constraints = (
+        np.array(gpu.get_power_management_limit_constraints()) / 1_000
+    )  # mW 2 W
+    print("Power Limits:", pl_constraints)
 
-    print("Memory Clocks: ", memory_clocks, "for", name)
-    run_per_mem = int(np.ceil(num_total_runs / len(memory_clocks)))
-    for mem in memory_clocks:
-        gpu_clocks = np.array(dev.get_supported_graphics_clocks(mem))
-        selected_idx = np.linspace(0, len(gpu_clocks) - 1, run_per_mem, dtype=np.int)
-        selected = gpu_clocks[selected_idx]
+    memory_clocks = gpu.get_supported_memory_clocks()
+    print("Clocks:")
+    for mem_clock in memory_clocks:
+        gpu_clocks = np.array(gpu.get_supported_graphics_clocks(mem_clock))
+        print("\t", mem_clock)
+        print("\t\t", gpu_clocks)
 
-        print("\t", gpu_clocks)
-        print("selected for", mem, "\t", selected)
-        tuples = [f"- [ {mem}, {x} ]" for x in selected]
-        print("\n".join(tuples))
+
+def print_info():
+    """Print available power and clock limits for all GPUs."""
+    with pynvml3.NVMLLib() as nvml:
+        for gpu in nvml.device:
+            print_info4gpu(gpu)
+
+
+if __name__ == "__main__":
+    print_info()

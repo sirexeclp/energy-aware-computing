b6273ba8fe1651fbc7f227c89ce4a310eb7ac3c3
Felix Grzelka <sirexeclp@gmail.com>
2022-09-05 19:04:52 +0200

    improve output

--- gpyjoules/info.py
+++ gpyjoules/info.py
@@ -7,17 +7,19 @@ def print_info4gpu(gpu: pynvml3.Device):
     """Print power and clock limits for the given GPU-Device."""
     print("GPU:", gpu.get_name())
 
-    pl_constraints = (
+    pl_min, pl_max = (
         np.array(gpu.get_power_management_limit_constraints()) / 1_000
     )  # mW 2 W
-    print("Power Limits:", pl_constraints)
+    print("Power Limits:")
+    print("\tmin:", pl_min)
+    print("\tmax:", pl_max)
 
     memory_clocks = gpu.get_supported_memory_clocks()
     print("Clocks:")
     for mem_clock in memory_clocks:
         gpu_clocks = np.array(gpu.get_supported_graphics_clocks(mem_clock))
-        print("\t", mem_clock)
-        print("\t\t", gpu_clocks)
+        print("\tmemory:", mem_clock)
+        print("\t\tgpu:", gpu_clocks)
 
 
 def print_info():
@@ -25,6 +27,7 @@ def print_info():
     with pynvml3.NVMLLib() as nvml:
         for gpu in nvml.device:
             print_info4gpu(gpu)
+            print()
 
 
 if __name__ == "__main__":
